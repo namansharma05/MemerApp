44c140b95db5ec75334c8a55cbe56d4e8002c5ee
Naman <namanks0511@gmail.com>
2024-01-31 13:33:26 +0530

    added ontap and improved User interface

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meme_generator/services/api.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:meme_generator/themes.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+
+  void _onTileClicked(int index) {
+    debugPrint("You tapped on item $index");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +29,24 @@ class HomeView extends StatelessWidget {
                 itemBuilder: (context, index) {
                   final meme = snapshot.data!.data!.memes![index];
                   // print(meme.name!);
-                  return GridTile(
-                    child: CachedNetworkImage(
-                      imageUrl: meme.url!,
-                      placeholder: (context, url) =>
-                          CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                    ),
+                  return Stack(
+                    children: [
+                      Card(
+                        color: MyThemes.lightColorScheme.onSurfaceVariant,
+                        child: InkResponse(
+                          onTap: () => launchUrl(Uri.parse(meme.url!)),
+                          child: Center(
+                            child: CachedNetworkImage(
+                              imageUrl: meme.url!,
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator(),
+                              errorWidget: (context, url, error) =>
+                                  Icon(Icons.error),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   );
                 },
               );
